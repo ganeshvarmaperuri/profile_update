4e7c488341785a8d8322937a6b8ebe54ec3f8cff
Ganesh Varma <48859806+ganeshvarmaperuri@users.noreply.github.com>
2020-04-29 06:49:03 +0530

    update signals

--- webapp/signals.py
+++ webapp/signals.py
@@ -10,4 +10,4 @@ def create_profile(sender, instance, created, **kwargs):
         Profile.objects.create(user=instance)
 
 def save_profile(sender, instance, **kwargs):
-    instance.profile.save()
+    instance.profile.save(**kwargs)
